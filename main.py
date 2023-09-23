b801f323c5477a5d7a9800de210f3f820381d551
kheynerz <sebastiancr44@gmail.com>
2023-09-22 18:33:32 -0600

    Pipfile

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2 
+import matplotlib.pyplot as plt 
+
+
+"""
+import tensorflow as tf 
+import os
+import pandas as pd
+import numpy as np
+"""
+
+
+#OPEN CV CAN be used to read images and crop faces
+
+if __name__ == "__main__":
+    img_array = cv2.imread('train/angry/Training_3908.jpg')
+    print(img_array)
+    plt.imshow(img_array)
