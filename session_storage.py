342af0662a42802a842d9a6522043f1c23d3165b
kheynerz <sebastiancr44@gmail.com>
2023-10-19 13:59:00 -0600

    Implementation of session storage

--- session_storage.py
+++ session_storage.py
@@ -1,31 +1,66 @@
 import json
-import os
-import time
+from os import makedirs, path
+from time import time, sleep
 
 from constants import STORAGE_FOLDER_NAME
 
+from app_settings import AppSettings
+
 class SessionStorage:
-    _currentFile = None
+    _current_file = None
+    _folder_path = None
+    _file_counter = 0
+    _timestamp = None
 
-    def init_session(self, name : str) -> None: 
+
+    @staticmethod
+    def init_session(name : str) -> None: 
         try:
-            if not os.path.exists(STORAGE_FOLDER_NAME):
-                os.makedirs(STORAGE_FOLDER_NAME)
-            
-            file_path = os.path.join(STORAGE_FOLDER_NAME, f"{name}-{int(time.time())}.dat")
-            open(file_path, 'wb')
-            SessionStorage._currentFile = file_path
+            if not path.exists(STORAGE_FOLDER_NAME):
+                makedirs(STORAGE_FOLDER_NAME)
+            timestamp = int(time())
+            folder_path = path.join(STORAGE_FOLDER_NAME, f"{name}-{timestamp}")
+            makedirs(folder_path)
+            SessionStorage._folder_path = folder_path
+            SessionStorage._create_session_file(timestamp)
+
         except Exception as e:
-            print(f"No se pudo crear la sesión: '{STORAGE_FOLDER_NAME}/{name}.dat': {str(e)}")
+            print(f"Error in session creation: {str(e)}")
 
-    def read_from_session(self, start  : str = '0', end : str = f'1000000000') -> None:
-        pass
+    @staticmethod
+    def _create_session_file(timestamp: int):
+        file_name = f"{SessionStorage._file_counter}-{timestamp}"
+        SessionStorage._file_counter += 1
 
-    def append_session(self, data) -> None:
-        if (SessionStorage._currentFile == None): return
-        with open(SessionStorage._currentFile, 'ab') as file:
-            data_with_timestamp = f"{int(time.time())}-{json.dumps(data)}\n"
-            data_bytes = data_with_timestamp.encode('utf-8')
+        file = open(path.join(SessionStorage._folder_path, file_name), 'wb')
+        file.close()
+        SessionStorage._current_file = file_name
+        SessionStorage._timestamp = timestamp
+
+    @staticmethod
+    def check_and_create_file():
+        storage = AppSettings()
+        STORAGE_CREATION_FILE_RATE = int(storage.get_app_setting('storageCreationFileRate'))
+
+        timestamp = int(time())
+        if (timestamp - SessionStorage._timestamp <= STORAGE_CREATION_FILE_RATE) : return
+        
+        #Calcular promedio del archivo
+
+        SessionStorage._create_session_file(timestamp)
+
+
+    @staticmethod
+    def append_data(data) -> None:
+        SessionStorage.check_and_create_file()
+      
+        current_file = SessionStorage._current_file
+        folder_path = SessionStorage._folder_path
+
+        if (current_file == None or folder_path == None): return
+
+        with open(path.join(folder_path, current_file), 'ab') as file:
+            data_bytes = f"{json.dumps(data)}\n".encode('utf-8')
             file.write(data_bytes)
-
+            file.close()
 
