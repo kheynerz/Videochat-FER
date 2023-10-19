d46f1507c257fa44b71c4001891ada0f542cdefe
kheynerz <sebastiancr44@gmail.com>
2023-10-18 22:51:46 -0600

    So project

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-import cv2 
-import matplotlib.pyplot as plt 
-
-
-"""
-import tensorflow as tf 
-import os
-import pandas as pd
-import numpy as np
-"""
-
-
-#OPEN CV CAN be used to read images and crop faces
 
 if __name__ == "__main__":
-    img_array = cv2.imread('train/angry/Training_3908.jpg')
-    print(img_array)
-    plt.imshow(img_array)
-    plt.show()
+    print("SO project")
