f6b09658cb83ad9d2edb300cd6e1470bf4ba0ebe
Guido Arce <guidoa@hrmango.com>
2023-11-09 23:22:50 -0600

    Add get image by monitor

--- ImageGetter/get_images.py
+++ ImageGetter/get_images.py
@@ -1,36 +1,15 @@
-from PIL import ImageGrab
-import win32api
 import time
-from screeninfo import get_monitors
 import mss
+from app_settings import AppSettings
 
-def capture_full_screen(seconds):
-    # # Obtener las dimensiones de la pantalla principal
-    # screen_width = win32api.GetSystemMetrics(0)
-    # screen_height = win32api.GetSystemMetrics(1)
-
-    # # Captura la pantalla completa
-    # img = ImageGrab.grab(bbox=(0, 0, screen_width, screen_height))
-    
-    # # Tiempo antes de tomar la siguiente captura
-    # time.sleep(seconds)
-
-    # connected_monitors = get_monitors()
-    # for i, screen in enumerate(connected_monitors):
-    #     bbox = (screen.x, screen.y, screen.x + screen.width, screen.y + screen.height)
-    #     img = ImageGrab.grab(bbox)
-    #     img.save(f"screen_{i + 1}.png")
-
+def capture_full_screen(screen_number):
     with mss.mss() as sct:
-        captured_monitors = set()
-        for i, monitor in enumerate(sct.monitors):
-            monitor_id = (
-                monitor["width"],
-                monitor["height"],
-                monitor["left"],
-                monitor["top"],
-            )
-            if monitor_id not in captured_monitors:
-                img = sct.shot(output=f"monitor_{i + 1}.png", mon=i)
-                captured_monitors.add(monitor_id)
-    #return img
+        for i, _ in enumerate(sct.monitors):
+            if(i + 1 == screen_number):
+                sct.shot(output=f"images/Screen_{i + 1}.png", mon=i)
+
+    #Load settings
+    settings = AppSettings()
+    settings.load_settings()
+    delay = int(settings.get_app_setting('screenshotRate'))
+    time.sleep(delay)
